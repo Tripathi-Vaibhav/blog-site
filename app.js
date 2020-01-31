d9820e8c61ac3966841584573942a85e1079ed00
Vaibhav <01vatri@gmail.com>
2020-02-01 01:44:10 +0530

    send p text to each page

--- app.js
+++ app.js
@@ -15,11 +15,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function(req, res) {
+  res.render("home", {start: homeStartingContent});
+});
 
+app.get("/about", function(req, res) {
+  res.render("about", {about: aboutContent});
+});
 
-
-
-
+app.get("/contact", function(req, res) {
+  res.render("contact", {contact: contactContent});
+});
 
 
 
