ce8795fcb83d2654ae26f34c80d40e2329972079
Vaibhav <01vatri@gmail.com>
2020-04-03 12:05:29 +0530

    connect to mongodb atlas and add procfile

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology:true});
+mongoose.connect("mongodb+srv://admin-vaibhav-blog:blog@cluster0-qxxke.mongodb.net/blogDB", {useNewUrlParser: true, useUnifiedTopology:true});
 
 const postSchema = {
   title: String,
@@ -90,8 +90,11 @@ app.get("/posts/:postId", function(req, res) {
 //   console.log("Succcessfully deleted the document !");
 // });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started successfully");
 });
